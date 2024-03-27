f12b1df33f8adaab899c32e18fa84f38d66ce398
tomerkor1985 <tomerkor1985@gmail.com>
2024-03-27 22:25:54 +0200

    fix login

--- source/functions.py
+++ source/functions.py
@@ -6,7 +6,7 @@ from playwright.sync_api import sync_playwright
 
 class BaseSetup:
     def __init__(self):
-        with open("../source/config.json") as con:
+        with open("source/config.json") as con:
             self.data = json.load(con)
             self.username = self.data['username']
             self.password = self.data['password']
@@ -19,7 +19,7 @@ class Login(BaseSetup):
 
     def connect_to_website(self, username=None, password=None):
         with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(headless=False)
+            browser = playwright.chromium.launch(headless=True)
             page = browser.new_page(no_viewport=True)
             page.goto(self.data['url'])
             un = page.get_by_placeholder('Username')
@@ -34,7 +34,6 @@ class Login(BaseSetup):
                 ps.fill(password)
             # Click on connect button
             page.locator('button', has_text='Login').click()
-            time.sleep(30)
 
     # def get_login_status(self):
     #     """ Return True if login success, False if login failed. """
